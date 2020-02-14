9e52ed34dda606b77b77856309c5f0a59da4ad9c
DenisAvilov <avilovd.a@gmail.com>
2020-02-14 15:01:51 +0200

    Aquarium
    Aquarium html\css

---
+++ Aquarium/js/scripts.js
@@ -0,0 +1,10 @@
+// $(document).ready(function () {
+//   $('.burger_trigger').click(function () {
+//       $('.menu').show(1250);
+//   });
+// });
+$(document).ready(function () {
+    $('.burger_trigger').click(function () {
+        $('.menu').show();
+    });
+});
