6fb9c3b57892fad54206aa2118cd2d3df505bf5c
DenisAvilov <avilovd.a@gmail.com>
2020-03-02 03:13:47 +0200

    a little better what was

--- Aquarium/js/scripts.js
+++ Aquarium/js/scripts.js
@@ -7,4 +7,10 @@ $(document).ready(function () {
     $('.burger_trigger').click(function () {
         $('.menu').show();
     });
+
+   
+        $('.owl-carousel').owlCarousel({
+            items:1,
+        });
+    
 });
